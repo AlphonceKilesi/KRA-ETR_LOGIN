b2f841ee0204c0b4b236c02bd7539a12b5896917
Alphonce Sapuda <157350705+AlphonceKilesi@users.noreply.github.com>
2024-02-20 17:02:04 +0300

    ok

--- src/app/etr-container/script.js
+++ src/app/etr-container/script.js
@@ -1,17 +1,60 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.querySelector('.form-box');
-    
-    form.addEventListener('click', function(event) {
-        const target = event.target;
-        const inputs = form.querySelectorAll('input[required]');
-        
-        if (!target.closest('input')) {
-            inputs.forEach(input => {
-                if (!input.value.trim()) {
-                    alert('This field is required');
-                    input.focus();
-                }
-            });
-        }
-    });
+const form = document.getElementById('myForm');
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  let hasError = false;
+
+
+  if (usernameInput.value.trim() === '') {
+    hasError = true;
+    usernameInput.classList.add('error');
+    document.getElementById('username_error').style.display = 'block';
+  } else if (!/^[a-zA-Z ]+$/.test(usernameInput.value)) {
+    hasError = true;
+    usernameInput.classList.add('error');
+    document.getElementById('username_error').style.display = 'block';
+    document.getElementById('username_error').textContent = 'Username must only contain letters and spaces.';
+  } else if (usernameInput.value.length < 4) {
+    hasError = true;
+    usernameInput.classList.add('error');
+    document.getElementById('username_error').style.display = 'block';
+    document.getElementById('username_error').textContent = 'Username must be at least 4 characters long.';
+  } else {
+    usernameInput.classList.remove('error');
+    document.getElementById('username_error').style.display = 'none';
+  }
+
+  
+  if (passwordInput.value.trim() === '') {
+    hasError = true;
+    passwordInput.classList.add('error');
+    document.getElementById('pass_error').style.display = 'block';
+  } else if (passwordInput.value.length < 8) {
+    hasError = true;
+    passwordInput.classList.add('error');
+    document.getElementById('pass_error').style.display = 'block';
+    document.getElementById('pass_error').textContent = 'Password must be at least 8 characters long!';
+  } else {
+    passwordInput.classList.remove('error');
+    document.getElementById('pass_error').style.display = 'none';
+  }
+
+  
+  if (hasError) {
+    event.preventDefault(); 
+  }
 });
+
+function switchToAdminLogin() {
+  const adminLink = document.querySelector('a[onclick="switchToAdminLogin()"]');
+  const form = document.getElementById('myForm');
+
+  
+  adminLink.style.display = 'none';
+
+  
+
+}
